7c16d524a5323ab008c506c183c17afd3034d2ef
Aleksi Postari <kulttuuri@users.noreply.github.com>
2014-07-19 17:12:58 +0300

    Continued developing 1.6.4 in it's own branch. Fixed so that panel is no longer off by 1 pixel.

---
+++ src/main/java/com/kulttuuri/inventoryscroll/proxy/clientProxy.java
@@ -0,0 +1,27 @@
+package com.kulttuuri.inventoryscroll.proxy;
+
+import org.lwjgl.input.Keyboard;
+
+import com.kulttuuri.quickhotbar.QuickHotbarEventHandler;
+
+import cpw.mods.fml.client.registry.ClientRegistry;
+import cpw.mods.fml.common.FMLCommonHandler;
+import cpw.mods.fml.common.registry.TickRegistry;
+import cpw.mods.fml.relauncher.Side;
+
+import net.minecraft.client.settings.KeyBinding;
+import net.minecraftforge.common.MinecraftForge;
+
+public class clientProxy extends CommonProxy
+{
+	@Override
+	public void registerEvents()
+	{
+		super.registerEvents();
+		
+		//FMLCommonHandler.instance()register(new InventoryScrollEventHandler());
+		//FMLCommonHandler.instance().re
+		TickRegistry.registerTickHandler(new QuickHotbarEventHandler(), Side.CLIENT);
+		MinecraftForge.EVENT_BUS.register(new QuickHotbarEventHandler());
+	}
+}
---
+++ src/main/java/com/kulttuuri/inventoryscroll/proxy/serverProxy.java
@@ -0,0 +1,10 @@
+package com.kulttuuri.inventoryscroll.proxy;
+
+public class serverProxy extends CommonProxy
+{
+	@Override
+	public void registerEvents()
+	{
+		super.registerEvents();
+	}
+}
