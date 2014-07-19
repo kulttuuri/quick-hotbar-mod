d4f4a8cb7318232ffe5fb4b88551488a80201ddc
Aleksi Postari <kulttuuri@users.noreply.github.com>
2014-07-19 22:56:20 +0300

    Changed version to 1.01.

---
+++ src/main/java/com/kulttuuri/quickhotbar/QuickHotbarModInfo.java
@@ -0,0 +1,11 @@
+package com.kulttuuri.quickhotbar;
+
+/**
+ * Global mod information.
+ * @author Kulttuuri
+ */
+public class QuickHotbarModInfo
+{
+    public static final String MODID = "quickhotbar";
+    public static final String VERSION = "1.01";
+}
