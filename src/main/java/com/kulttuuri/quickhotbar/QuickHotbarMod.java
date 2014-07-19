bf8628be4ce57b5620e1624a77c345e8ccca6a13
Aleksi Postari <kulttuuri@users.noreply.github.com>
2014-07-19 22:46:33 +0300

    Added configurable file settings. Mod now prints out welcome message for user. Added so that user can scroll inventory slots with keyboard keys too. Added support for immediately bringing out the item preview GUI.

--- src/main/java/com/kulttuuri/quickhotbar/QuickHotbarMod.java
+++ src/main/java/com/kulttuuri/quickhotbar/QuickHotbarMod.java
@@ -3,6 +3,7 @@ package com.kulttuuri.quickhotbar;
 import org.lwjgl.input.Keyboard;
 
 import com.kulttuuri.quickhotbar.proxy.IProxy;
+import com.kulttuuri.quickhotbar.settings.SettingsClient;
 
 import net.minecraft.init.Blocks;
 import cpw.mods.fml.common.Mod;
@@ -10,6 +11,8 @@ import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.Mod.Instance;
 import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.relauncher.Side;
 
 @Mod(modid = QuickHotbarModInfo.MODID, version = QuickHotbarModInfo.VERSION)
 public class QuickHotbarMod
@@ -17,8 +20,19 @@ public class QuickHotbarMod
     @Instance(QuickHotbarModInfo.MODID)
     public static QuickHotbarMod instance;
     
+    public static SettingsClient clientSettings = new SettingsClient();
+    
     @SidedProxy(clientSide = "com.kulttuuri.quickhotbar.proxy.clientProxy", serverSide = "com.kulttuuri.quickhotbar.proxy.serverProxy")
     public static IProxy proxy;
+    
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+    	if (event.getSide() == Side.CLIENT)
+    	{
+    		clientSettings.loadSettingsFromFile(event.getSuggestedConfigurationFile());
+    	}
+    }
     
     @EventHandler
     public void init(FMLInitializationEvent event)
