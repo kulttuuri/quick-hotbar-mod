d1588fb793646accbc21fc1412443553383d2536
Aleksi_Mac <aleksi@teachergaming.com>
2014-08-24 22:45:27 +0300

    Moved GuiSettingsBase.currentGuiScreen initialization to clientProxy to not crash the game when mod starts in server. Now default mode (row or column switching) is also reset when joining a server. Added in announce message mention about the menu keybinding. Removed debug console message.

--- src/main/java/com/kulttuuri/quickhotbar/proxy/clientProxy.java
+++ src/main/java/com/kulttuuri/quickhotbar/proxy/clientProxy.java
@@ -1,5 +1,7 @@
 package com.kulttuuri.quickhotbar.proxy;
 
+import com.kulttuuri.quickhotbar.gui.GuiSettingsBase;
+import com.kulttuuri.quickhotbar.gui.GuiSettingsInformation;
 import org.lwjgl.input.Keyboard;
 
 import com.kulttuuri.quickhotbar.QuickHotbarEventHandler;
@@ -19,5 +21,7 @@ public class clientProxy extends CommonProxy
 		
 		FMLCommonHandler.instance().bus().register(new QuickHotbarEventHandler());
 		MinecraftForge.EVENT_BUS.register(new QuickHotbarEventHandler());
+
+        GuiSettingsBase.currentGuiScreen = new GuiSettingsInformation();
 	}
 }
