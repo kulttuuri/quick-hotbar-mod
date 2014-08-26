60956f8a54563268cfb5e3722a286a269b4ca22b
Aleksi_Mac <aleksi@teachergaming.com>
2014-08-26 23:28:46 +0300

    URL now opens in browser. Fixed bug when using keyboard scrolling in column mode when having only clientside support, it would not switch in the correct column. Fixed bug when using reversed scrolling in column mode it would not switch the correct column. Went through all project classes and removed unnecessary imports.

--- src/main/java/com/kulttuuri/quickhotbar/packets/PacketAnnounceServerAssist.java
+++ src/main/java/com/kulttuuri/quickhotbar/packets/PacketAnnounceServerAssist.java
@@ -1,13 +1,10 @@
 package com.kulttuuri.quickhotbar.packets;
 
 import com.kulttuuri.quickhotbar.QuickHotbarMod;
-import cpw.mods.fml.common.network.ByteBufUtils;
 import cpw.mods.fml.common.network.simpleimpl.IMessage;
 import cpw.mods.fml.common.network.simpleimpl.IMessageHandler;
 import cpw.mods.fml.common.network.simpleimpl.MessageContext;
 import io.netty.buffer.ByteBuf;
-import net.minecraft.entity.player.InventoryPlayer;
-import net.minecraft.item.ItemStack;
 
 public class PacketAnnounceServerAssist implements IMessage
 {
